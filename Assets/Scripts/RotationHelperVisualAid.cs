ca11c6d4731d52b1a9fc243bde9b09ab824cb898
Alex Burnley <alex.burnley@colorado.edu>
2022-11-02 17:09:31 -0600

    Added rotation helper

---
+++ Assets/Scripts/RotationHelperVisualAid.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationHelperVisualAid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: change visibility based on proximity to hand
+
+        // TODO: detect hand grabbing visual aid and notify parent   
+    }
+}
