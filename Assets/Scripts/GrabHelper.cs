7835868ea9b03abaf9a30405f5a5379d5976f6bc
Alex Burnley <alex.burnley@colorado.edu>
2022-12-06 22:51:59 -0700

    Added GrabHelper for better grabbing mechanics. Moved character controller and interactable items to separate physics layers to avoid issues. This causes player to be able to walk through interactable items

---
+++ Assets/Scripts/GrabHelper.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabHelper : MonoBehaviour
+{
+
+    public GameObject anchor;
+
+    bool currentlyHolding;
+
+    UnityEngine.XR.Interaction.Toolkit.IXRHoverInteractable hoverObject;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentlyHolding = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!currentlyHolding && hoverObject != null) {
+            transform.position = hoverObject.transform.position;
+            transform.rotation = hoverObject.transform.rotation;
+        }
+    }
+
+
+    public void OnHoverEntered(UnityEngine.XR.Interaction.Toolkit.HoverEnterEventArgs args) {
+
+        if(!currentlyHolding) {
+            hoverObject = args.interactableObject;
+            transform.position = hoverObject.transform.position;
+            transform.rotation = hoverObject.transform.rotation;
+            Debug.Log("Hover entered");
+        }
+    }
+
+    public void OnHoverExited(UnityEngine.XR.Interaction.Toolkit.HoverExitEventArgs args) {
+        hoverObject = null;
+    }
+
+    public void OnSelectEntered(UnityEngine.XR.Interaction.Toolkit.SelectEnterEventArgs args) {
+        
+        currentlyHolding = true;
+    }
+
+    public void OnSelectExited(UnityEngine.XR.Interaction.Toolkit.SelectExitEventArgs args) {
+        currentlyHolding = false;
+    }
+}
